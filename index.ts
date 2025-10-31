733191e386ce4925426043d380a21601a6cd3d4d
jhpark <jhpark@towncar.co.kr>
2025-10-31 13:26:00 +0900

    [fix] 임포트 에러 픽스

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Ddu64, Custom64, Ddu128, Ddu512, Ddu1024 } from "./src";
+import { Ddu64} from "./src";
 
 export default Ddu64;
-export { Ddu64, Custom64, Ddu128, Ddu512, Ddu1024 };
+export { Ddu64 };
